4d14012dde0bfdf15036ffc403dedea7356be5a0
ivanwritescode <ivanmejico.dev@gmail.com>
2024-03-11 16:18:11 +0800

    Added check

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -44,10 +44,14 @@ export const getMessages = async (req, res) => {
 
         const conversation = await Conversation.findOne({
             participants: { $all: [senderId, userToChatId] },
-        }).populate("messages");
+        }).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
 
-        res.status(200).json(conversation.messages);
+        if(!conversation) return res.status(200).json([]);
+
+        const messages = conversation.messages;
+
+        res.status(200).json(messages);
     } catch (error) {
-        console.log("Error in getMes");
+        console.log("Error in getMessages");
     }
 }
