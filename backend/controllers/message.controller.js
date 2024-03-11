ab1a5645688232b9bd4bbfc45cb6372c855a4506
ivanwritescode <ivanmejico.dev@gmail.com>
2024-03-11 16:12:27 +0800

    Added code for getting messages betwee current logged-in user and target

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -36,3 +36,18 @@ export const sendMessage = async (req, res) => {
     }
 
 }
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages");
+
+        res.status(200).json(conversation.messages);
+    } catch (error) {
+        console.log("Error in getMes");
+    }
+}
